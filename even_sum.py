e8cc4a5c22ec804a2b0da9225ef91f2da1c66005
Bipin Raut <bipinraut704@gmail.com>
2025-09-07 06:43:58 +0545

    Update even_sum.py

--- even_sum.py
+++ even_sum.py
@@ -3,7 +3,7 @@ def main():
     if n < 0:
         print("Number not a positive number");
         return
-    print(f"Total sum of even numbers from range 1 to {n}:", sum_of_evens(n))
+    print(f"Total sum{n}:", sum_of_evens(n))
 
 def sum_of_evens(n):
     res = 0
