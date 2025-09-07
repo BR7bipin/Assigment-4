c221d1b9b2c6f372c0c19fc3d00237a0af9e25b1
Bipin Raut <bipinraut704@gmail.com>
2025-09-07 06:39:57 +0545

    Update daycheck.py

--- daycheck.py
+++ daycheck.py
@@ -1,32 +1,22 @@
-# Main Function
 def main():
-    # Get day_num from the user input
-    day_num = int(input("Enter a number between 1-7: "))
-    # Display the return value of day_checker function
+    day_num = input("Enter a number between 1-7: "))
     print(day_checker(day_num))
-
-# day_checker function
-# @params day_num: number of the correspoding day
-# returns the name of the day corresponding to the day_num, 
-# otherwise returns "Invalid Input" as default
 def day_checker(day_num):
-    # Match case statement on day_num parameter
     match day_num:
-        case 1: 
-            return "Monday" # Returns 'Monday' on day_name = 1
-        case 2:
-            return "Tuesday" # Returns 'Tuesday' on day_name = 2
-        case 3:
-            return "Wednesday" # Returns 'Wednesday' on day_name = 3
-        case 4:
-            return "Thursday" # Returns 'Thursday' on day_name = 4
-        case 5:
-            return "Friday" # Returns 'Friday' on day_name = 5
-        case 6:
-            return "Saturday" # Returns 'Saturday' on day_name = 6
-        case 7:
-            return "Sunday" # Returns 'Sunday' on day_name = 7
+        case "1": 
+            return "Monday"
+        case "2":
+            return "Tuesday"
+        case "3":
+            return "Wednesday"
+        case "4":
+            return "Thursday"
+        case "5":
+            return "Friday"
+        case "6":
+            return "Saturday"
+        case "7":
+            return "Sunday"
         case _:
-            return "Invalid Input" # Returns 'Invalid Input' as the default case
-
-main()  # Run the main function
+            return "Invalid Input" 
+main()
